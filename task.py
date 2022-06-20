5dfa3b8e47249981d96b11f2436caf41c847daa2
Devon Ankar <devon.ankar@gmail.com>
2022-06-19 23:46:18 -0400

    Add single latest data file

--- task.py
+++ task.py
@@ -1,3 +1,4 @@
+import json
 import re
 from time import sleep
 
@@ -8,18 +9,28 @@ from bs4 import BeautifulSoup
 from messaging import send_email
 
 _POLLS_PATTERN = '#MI|#..(Sen|SEN|Gov|GOV) General'
+_GENERIC_BALLOT_DATA_FILEPATH = 'data/generic_ballot_averages.csv'
+
+
+def _read_latest() -> dict:
+    data_from_file = json.load(open('data/latest.txt'))
+    return data_from_file
+
+
+def _update_latest(new_data: dict) -> None:
+    data_from_file = _read_latest()
+    data_from_file.update(new_data)
+    json.dump(data_from_file, open('data/latest.txt', 'w'))
 
 
 def _get_gcb(session: requests.Session) -> str:
-    filepath = 'data/generic_ballot_averages.csv'
-
-    existing_content = open(filepath, 'rb').read()
+    existing_content = open(_GENERIC_BALLOT_DATA_FILEPATH, 'rb').read()
     new_content = session.get('https://projects.fivethirtyeight.com/polls/data/generic_ballot_averages.csv').content
     if existing_content == new_content:
         return ''
-    open(filepath, 'wb').write(new_content)
+    open(_GENERIC_BALLOT_DATA_FILEPATH, 'wb').write(new_content)
 
-    data = pd.read_csv(filepath, usecols=['candidate', 'pct_estimate', 'election'])
+    data = pd.read_csv(_GENERIC_BALLOT_DATA_FILEPATH, usecols=['candidate', 'pct_estimate', 'election'])
     data = data[data.election == '2022-11-08'].drop(columns=['election']).iloc[-2:]
     data.candidate = data.candidate.apply(lambda x: x[0])
     estimates = data.groupby('candidate').pct_estimate.sum()
@@ -27,10 +38,11 @@ def _get_gcb(session: requests.Session) -> str:
     data.pct_estimate = data.pct_estimate.apply(lambda x: round(x, 2))
     estimates = data.groupby('candidate').pct_estimate.sum()
 
-    summary = 'D: {D}\nR: {R}\nR+{difference}'.format(difference=difference, **estimates)
-    if summary != open('data/gcb_summary.txt').read():
-        return summary
-    return ''
+    output = 'D: {D}\nR: {R}\nR+{difference}'.format(difference=difference, **estimates)
+    if output == _read_latest()['gcb']:
+        return ''
+    _update_latest(dict(gcb=output))
+    return output
 
 
 def _get_feed(session: requests.Session) -> str:
@@ -40,18 +52,18 @@ def _get_feed(session: requests.Session) -> str:
         list(map(lambda x: item.find(x).text, ('title', 'link', 'pubDate')))
         for item in feed.find_all('item') if 'forecast' in item.find('title').text.lower()
     ]
-    summary = '\n\n___\n\n'.join('\n'.join(i) for i in data)
-    if summary != open('data/feed_summary.txt').read():
-        return summary
-    return ''
+    output = '\n\n___\n\n'.join('\n'.join(i) for i in data)
+    if output == _read_latest()['feed']:
+        return ''
+    _update_latest(dict(feed=output))
+    return output
 
 
 def _get_polls() -> str:
-    previous_latest_link = open('data/polls.txt').read()
     response = requests.get('https://nitter.net/PollTrackerUSA/rss')
     feed = BeautifulSoup(response.text, 'xml')
     tweets = feed.select('item')
-    open('data/polls.txt', 'w').write(tweets[0].find('link').text)
+    previous_latest_link = _read_latest()['polls']
 
     polls = []
     for tweet in tweets:
@@ -61,6 +73,7 @@ def _get_polls() -> str:
         if re.search(_POLLS_PATTERN, title):
             polls.append(dict(title=title, pubdate=pubdate))
 
+    _update_latest(dict(polls=tweets[0].find('link').text))
     return '\n\n'.join('{title}\n\nPubDate: {pubdate}'.format(**poll) for poll in polls)
 
 
@@ -72,13 +85,11 @@ def main():
 
     if gcb_summary := _get_gcb(session):
         messages.append(gcb_summary)
-        open('data/gcb_summary.txt', 'w').write(gcb_summary)
 
     sleep(1)
 
     if feed_summary := _get_feed(session):
         messages.append(feed_summary)
-        open('data/feed_summary.txt', 'w').write(feed_summary)
 
     session.close()
 

8abe069e192278be68bec93cb24ff60c02515449
Devon Ankar <devon.ankar@gmail.com>
2022-08-08 17:44:31 -0400

    Revert "Create global _TOPLINE_FILENAMES"
    This reverts commit a0b5b486e113610a07b5921a87b18afb36372680.

--- task.py
+++ task.py
@@ -13,11 +13,6 @@ from twilio.rest import Client
 
 _CONFIG = configparser.ConfigParser()
 _CONFIG.read('config.ini')
-_TOPLINE_FILENAMES = dict(
-    senate='senate_state_toplines_2022.csv',
-    house='house_district_toplines_2022.csv',
-    governor='governor_state_toplines_2022.csv',
-)
 
 
 def _send_email(subject: str, body: str) -> None:
@@ -142,7 +137,11 @@ def _get_seat_forecasts(session: requests.Session, chamber: str) -> str:
     if not seats:
         return ''
 
-    data_filename = _TOPLINE_FILENAMES[chamber]
+    data_filename = dict(
+        senate='senate_state_toplines_2022.csv',
+        house='house_district_toplines_2022.csv',
+        governor='governor_state_toplines_2022.csv',
+    )[chamber]
     data_filepath = f'data/{data_filename}'
     url = f'https://projects.fivethirtyeight.com/2022-general-election-forecast-data/{data_filename}'
     open(data_filepath, 'wb').write(session.get(url).content)

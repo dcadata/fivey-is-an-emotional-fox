5d7afa6809105705c727d256155faa05dce3c6f4
Devon Ankar <devon.ankar@gmail.com>
2022-09-06 15:28:24 -0400

    Update compare_forecast_expressions.py

--- compare_forecast_expressions.py
+++ compare_forecast_expressions.py
@@ -1,38 +1,27 @@
-from time import sleep
-
 import pandas as pd
 
-from task import _FTE_FORECAST_BASE_URL, _DISTRICT_TOPLINE_FILENAMES
+from task import _DISTRICT_TOPLINE_FILENAMES
 
 
-def compare_forecast_expressions(chamber: str) -> None:
+def rejoin_expressions(chamber: str) -> pd.DataFrame:
     chamber = chamber.lower()
 
-    df = pd.read_csv(f'{_FTE_FORECAST_BASE_URL}{_DISTRICT_TOPLINE_FILENAMES[chamber]}', usecols=[
-        'district', 'expression', 'winner_Dparty']).drop_duplicates(subset=[
-        'district', 'expression'], keep='first').rename(columns=dict(winner_Dparty='probD'))
+    df = pd.read_csv(f'data/{_DISTRICT_TOPLINE_FILENAMES[chamber]}', usecols=[
+        'district', 'forecastdate', 'expression', 'winner_Dparty']).rename(columns=dict(winner_Dparty='probD'))
     df.probD = df.probD.round(2)
 
-    func = lambda x: df[df.expression == x].drop(columns='expression')
-    merged = func('_deluxe').merge(func('_classic'), on='district', suffixes=('', '_classic')).merge(
-        func('_lite'), on='district', suffixes=('', '_lite'))
+    _separate_expression = lambda x: df[df.expression == x].drop(columns='expression')
+    merged = (
+        _separate_expression('_deluxe')
+            .merge(_separate_expression('_classic'), on=['district', 'forecastdate'], suffixes=('', '_classic'))
+            .merge(_separate_expression('_lite'), on=['district', 'forecastdate'], suffixes=('', '_lite'))
+    )
     merged = merged.rename(columns=dict(district='seat', probD='probD_deluxe'))
+
+    merged.forecastdate = merged.forecastdate.apply(lambda x: pd.to_datetime(x).date())
+    merged = merged.sort_values('forecastdate')
 
     if chamber in ('senate', 'governor'):
         merged.seat = merged.seat.apply(lambda x: x[:2])
 
-    merged['probD_deluxe_minus_lite'] = merged.probD_deluxe - merged.probD_lite
-    merged['probD_deluxe_minus_lite_abs'] = merged.probD_deluxe_minus_lite.apply(abs).round(2)
-    merged.probD_deluxe_minus_lite = merged.probD_deluxe_minus_lite.round(2)
-    merged = merged.sort_values('probD_deluxe_minus_lite_abs', ascending=False)
-    merged.to_csv(f'forecast_expression_comparisons/{chamber}.csv', index=False)
-
-
-def main():
-    for chamber in _DISTRICT_TOPLINE_FILENAMES.keys():
-        compare_forecast_expressions(chamber)
-        sleep(2)
-
-
-if __name__ == '__main__':
-    main()
+    return merged

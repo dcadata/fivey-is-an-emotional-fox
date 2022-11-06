b1098e3a00db846fb12187683c2952a27a2e82a0
Devon Ankar <devon.ankar@gmail.com>
2022-11-06 12:46:59 -0500

    Clean up date filtering

--- gcb_polls_movement.py
+++ gcb_polls_movement.py
@@ -1,5 +1,4 @@
 import datetime
-import re
 
 import pandas as pd
 
@@ -9,23 +8,16 @@ FOLDER = 'gcb_movement/'
 def _read_gcb_polls(additional_cols: list = None) -> pd.DataFrame:
     df = pd.read_csv('data/generic_ballot_polls.csv', usecols=[
         'poll_id', 'sponsors', 'display_name', 'fte_grade', 'methodology', 'partisan', 'population',
-        'election_date', 'start_date', 'end_date',
+        'start_date', 'end_date',
         'dem', 'rep',
         *(additional_cols if additional_cols else []),
     ])
     return df
 
 
-def _filter_gcb_polls(df: pd.DataFrame) -> pd.DataFrame:
-    df = df[(df.election_date == '11/8/22') & df.start_date.str.endswith(('/21', '/22'))].drop(
-        columns='election_date')
-    return df
-
-
 def _normalize_gcb_polls(df: pd.DataFrame) -> pd.DataFrame:
-    def _convert_date_str_to_dttm(x) -> datetime.date:
-        x = re.sub('/2$', '/202', x, count=1)
-        return pd.to_datetime(x).date()
+    def _convert_date_str_to_dttm(date_str) -> datetime.date:
+        return datetime.datetime.strptime(date_str, '%m/%d/%y').date()
 
     df.population = df.population.fillna('Not Specified').str.upper()
     df.fte_grade = df.fte_grade.fillna('Unrated')
@@ -38,8 +30,7 @@ def _normalize_gcb_polls(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def _split_on_date_and_merge_again(
-        df: pd.DataFrame, split_date: tuple, first_date: tuple = (2022, 1, 1)) -> pd.DataFrame:
+def _split_on_date_and_merge_again(df: pd.DataFrame, first_date: tuple, split_date: tuple) -> pd.DataFrame:
     data = df.copy()
     merge_cols = ['pollsterName', 'fteGrade', 'sponsor', 'population', 'partisan']
 
@@ -64,26 +55,28 @@ def _split_on_date_and_merge_again(
     return result
 
 
-def _split_on_date_and_merge_again_and_save(df: pd.DataFrame, label: str, *args, **kwargs) -> None:
-    _split_on_date_and_merge_again(df, *args, **kwargs).to_csv(f'{FOLDER}{label}.csv', index=False)
+def _split_on_date_and_merge_again_and_save(df: pd.DataFrame, label: str, *args) -> None:
+    _split_on_date_and_merge_again(df, *args).to_csv(f'{FOLDER}{label}.csv', index=False)
     return
 
 
 def create_gcb_polls_movement_trackers(df: pd.DataFrame) -> None:
-    df = _normalize_gcb_polls(_filter_gcb_polls(df))
-    _split_on_date_and_merge_again_and_save(df, '1-Dobbs (YTD split at 6.24)', (2022, 6, 24))
-    _split_on_date_and_merge_again_and_save(df, '2-Student Loan Forgiveness (6.24-8.24 vs 8.24-Today)', first_date=(
-        2022, 6, 24), split_date=(2022, 8, 24))
+    df = _normalize_gcb_polls(df)
+
+    _split_on_date_and_merge_again_and_save(
+        df, '1-Dobbs (YTD split at 6.24)', (2022, 1, 1), (2022, 6, 24))
+    _split_on_date_and_merge_again_and_save(
+        df, '2-Student Loan Forgiveness (6.24-8.24 vs 8.24-Today)', (2022, 6, 24), (2022, 8, 24))
     return
 
 
 def create_gcb_polls_trimmed() -> None:
-    df = _normalize_gcb_polls(_filter_gcb_polls(_read_gcb_polls(['cycle'])))[[
+    df = _normalize_gcb_polls(_read_gcb_polls(['cycle']))[[
         'pollsterName', 'sponsor', 'fteGrade', 'methodology', 'start_date', 'end_date', 'population', 'partisan',
-        'dem', 'rep', 'cycle',
+        'dem', 'rep',
     ]]
-    df = df[df.start_date.apply(lambda x: x.year) == 2022].rename(columns=dict(
-        start_date='startDate', end_date='endDate'))
+    df = df.rename(columns=dict(start_date='startDate', end_date='endDate'))
+    df['endYear'] = df.endDate.apply(lambda x: x.year)
     df['endMonth'] = df.endDate.apply(lambda x: x.month)
     for col in ('startDate', 'endDate'):
         df[col] = df[col].apply(lambda x: x.strftime('%m/%d/%Y'))

9a469c8a3a053290046f199e1ce68f4a5ef9cb87
Devon Ankar <devon.ankar@gmail.com>
2022-11-06 13:45:12 -0500

    Turn off trackers

--- task.py
+++ task.py
@@ -87,7 +87,7 @@ def _get_gcb_average(session: requests.Session) -> str:
 
     if use_gcb_tracking:
         _refresh_gcb_rolling_means()
-        _refresh_gcb_polls_trackers(session)
+        # _refresh_gcb_polls_trackers(session)
         gcb_polls_movement.create_gcb_polls_trimmed()
 
     change_from_previous = unrounded_lead - _read_latest().get('gcb_average', 0)
